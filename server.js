0e9ffcf1ee754b31e161bc4ddf72b8517b4cd820
Andrew <andrew-oko-odion@gmail.com>
2018-11-18 09:30:02 +0100

    update costom server port

--- server.js
+++ server.js
@@ -19,9 +19,9 @@ app.prepare()
             return handle(req, res);
         });
 
-        server.listen(3002, err => {
+        server.listen(3000, err => {
             if (err) throw err;
-            console.log('> Ready on http://localhost:3002');
+            console.log('> Ready on http://localhost:3000');
         });
     })
     .catch(ex => {
